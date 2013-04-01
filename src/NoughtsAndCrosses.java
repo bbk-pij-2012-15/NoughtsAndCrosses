0b9daafd25795e6bb44b53c46272f4780a642e4d
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-04-01 19:10:37 +0100

    made method to let player choose their symbol

--- src/NoughtsAndCrosses.java
+++ src/NoughtsAndCrosses.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class NoughtsAndCrosses
 {
+    char playerSymbol;
+    char computerSymbol;
+
     public static void main(String[] args)
     {
         NoughtsAndCrosses game = new NoughtsAndCrosses();
@@ -18,6 +23,40 @@ public class NoughtsAndCrosses
 
     public void play()
     {
+        System.out.println("Welcome to Noughts and Crosses! Will you be X or O? ");
+        getSymbols();
+        System.out.println("You will be " + playerSymbol + ", the computer will be " + computerSymbol + ". Let's Play!");
+    }
 
+    public char getSymbols()
+    {
+        while (true)
+        {
+            Scanner in = new Scanner(System.in);
+            String input = in.next();
+            if (input.length() > 1)
+            {
+                System.out.println("Please choose one symbol, either 'x' or 'o'!");
+            }
+            else if (input.charAt(0) != 'o' || input.charAt(0) != 'x' ||
+                    input.charAt(0) != 'O' || input.charAt(0) != 'X')
+            {
+                System.out.println("Selection must be either an 'x' or an 'o'!");
+            }
+            else
+            {
+                if (input.charAt(0) == 'o' || input.charAt(0) == 'O')
+                {
+                    playerSymbol = 'O';
+                    computerSymbol = 'X';
+                }
+                else
+                {
+                    playerSymbol = 'X';
+                    computerSymbol = 'O';
+                }
+                return playerSymbol;
+            }
+        }
     }
 }
