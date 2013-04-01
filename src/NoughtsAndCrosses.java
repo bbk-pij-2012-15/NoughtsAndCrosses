51e8ca34b81c96ff5a3374a0b317cc1356ab4cf6
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-04-01 18:10:44 +0100

    initial commit

---
+++ src/NoughtsAndCrosses.java
@@ -0,0 +1,23 @@
+public class NoughtsAndCrosses
+{
+    public static void main(String[] args)
+    {
+        NoughtsAndCrosses game = new NoughtsAndCrosses();
+        game.play();
+    }
+
+    public NoughtsAndCrosses()
+    {
+        this.setUp();
+    }
+
+    public void setUp()
+    {
+
+    }
+
+    public void play()
+    {
+
+    }
+}
