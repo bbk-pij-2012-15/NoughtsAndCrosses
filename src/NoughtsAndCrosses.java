2023216554eaec91d2bd76b695fea2ced464c77e
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-04-02 16:28:16 +0100

    modified board, chose unicode chars and added a random decider of who goes first

--- src/NoughtsAndCrosses.java
+++ src/NoughtsAndCrosses.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class NoughtsAndCrosses
@@ -6,11 +7,12 @@ public class NoughtsAndCrosses
     char computerSymbol;
     char[] board = new char[9];     // array to hold what is on each square of the board
     boolean over = false;
+    Random randomGenerator = new Random();
 
     public static void main(String[] args)
     {
         NoughtsAndCrosses game = new NoughtsAndCrosses();
-        game.play();
+        game.start();
     }
 
     public NoughtsAndCrosses()
@@ -20,20 +22,55 @@ public class NoughtsAndCrosses
 
     public void setUp()
     {
-
+        for (int i = 0; i < board.length; i++)
+        {
+            board[i] = ' ';
+        }
     }
 
-    public void play()
+    public void start()
     {
         System.out.println("Welcome to Noughts and Crosses! Will you be X or O? ");
         getSymbols();
         System.out.println("You will be " + playerSymbol + ", the computer will be " + computerSymbol + ". Let's Play!");
+        boolean turn = whoFirst();
+        play(turn);
+    }
+
+    public void play(boolean playerGoesFirst)
+    {
+        if (playerGoesFirst)
+        {
+
+        }
+        else
+        {
+
+        }
+
+
 
         while (!over)
         {
             drawGrid(board);
+
+
             over = true;
         }
+    }
+
+    public boolean whoFirst()
+    {
+        boolean playerFirst = randomGenerator.nextBoolean();
+        if (playerFirst)
+        {
+            System.out.println("The computer will go first... ");
+        }
+        else
+        {
+            System.out.println("You will go first... ");
+        }
+        return playerFirst;
     }
 
     public void getSymbols()
@@ -50,13 +87,13 @@ public class NoughtsAndCrosses
             {
                 if (input.charAt(0) == 'o' || input.charAt(0) == 'O')
                 {
-                    playerSymbol = 'O';
-                    computerSymbol = 'X';return;
+                    playerSymbol = '\u25CF';
+                    computerSymbol = '\u2718';return;
                 }
                 else if (input.charAt(0) == 'x' || input.charAt(0) == 'X')
                 {
-                    playerSymbol = 'X';
-                    computerSymbol = 'O';return;
+                    playerSymbol = '\u2718';
+                    computerSymbol = '\u25CF';return;
                 }
                 else
                 {
@@ -68,16 +105,13 @@ public class NoughtsAndCrosses
 
     public void drawGrid(char[] board)
     {
-        System.out.println("   |   |");
-        System.out.println(" " + board[6] + " | " + board[7] + " | " + board[8]);
-        System.out.println("   |   |");
-        System.out.println("-----------");
-        System.out.println("   |   |");
-        System.out.println(" " + board[3] + " | " + board[4] + " | " + board[5]);
-        System.out.println("   |   |");
-        System.out.println("-----------");
-        System.out.println("   |   |");
-        System.out.println(" " + board[0] + " | " + board[1] + " | " + board[2]);
-        System.out.println("   |   |");
+        System.out.println("    |   |");
+        System.out.println("  " + board[6] + " | " + board[7] + " | " + board[8]);
+        System.out.println("-------------");
+        System.out.println("  " + board[3] + " | " + board[4] + " | " + board[5]);
+        System.out.println("    |   |");
+        System.out.println("-------------");
+        System.out.println("  " + board[0] + " | " + board[1] + " | " + board[2]);
+        System.out.println("    |   |");
     }
 }
